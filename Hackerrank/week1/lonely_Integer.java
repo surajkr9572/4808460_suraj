736b1f73fda72c534cbfcb9d13f32bba16990ea0
suraj kumar <surajsingh43006@gmail.com>
2025-08-05 16:24:53 +0530

    lonely Intege

--- Hackerrank/week1/lonely_Integer.java
+++ Hackerrank/week1/lonely_Integer.java
@@ -1,17 +1,27 @@
 public static int lonelyinteger(List<Integer> a) {
-        Map<Integer,Integer>mp=new HashMap();
-        for(int i=0;i<a.size();i++){
-            if(mp.containsKey(a.get(i))){
-                mp.put(a.get(i),mp.get(a.get(i))+1);
-            }else{
-                mp.put(a.get(i),1);
-            }
+    Map<Integer, Integer> mp = new HashMap();
+    for (int i = 0; i < a.size(); i++) {
+        if (mp.containsKey(a.get(i))) {
+            mp.put(a.get(i), mp.get(a.get(i)) + 1);
+        } else {
+            mp.put(a.get(i), 1);
         }
+    }
+    int cnt = 0;
+    for (Map.Entry<Integer, Integer> e : mp.entrySet()) {
+        if (e.getValue() == 1) {
+            cnt = e.getKey();
+        }
+    }
+    return cnt;
+
+}
+    
+    --------------------------OR------------------------
+    public static int lonelyinteger(List<Integer> a) {
         int cnt = 0;
-        for (Map.Entry<Integer, Integer> e : mp.entrySet()) {
-            if (e.getValue() == 1) {
-                cnt = e.getKey();
-            }
+        for (Integer i : a) {
+            cnt = cnt ^ i;
         }
         return cnt;
 
