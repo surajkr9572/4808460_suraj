3b0e967baf6765e84202b0aed406fa7747216628
suraj kumar <surajsingh43006@gmail.com>
2025-08-07 12:31:17 +0530

    Zig Zag Sequence

---
+++ Hackerrank/week2/Zig Zag Sequence.java
@@ -0,0 +1,24 @@
+public static void findZigZagSequence(int [] a, int n){
+        Arrays.sort(a);
+        int mid = (n + 1)/2-1;
+        int temp = a[mid];
+        a[mid] = a[n - 1];
+        a[n - 1] = temp;
+        int st = mid + 1;
+        int ed = n - 2;
+        while(st <= ed){
+            temp = a[st];
+            a[st] = a[ed];
+            a[ed] = temp;
+            st = st + 1;
+            ed = ed - 1;
+        }
+        int st = mid + 1;
+        int ed = n - 2;
+        while(st <= ed){
+            temp = a[st];
+            a[st] = a[ed];
+            a[ed] = temp;
+            st = st + 1;
+            ed = ed - 1;
+        }
