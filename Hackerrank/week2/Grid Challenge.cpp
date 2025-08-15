f4be80e0f204c193fe3e51b322b0a985ea38d92f
suraj kumar <surajsingh43006@gmail.com>
2025-08-15 15:49:37 +0530

    Grid Challenge

---
+++ Hackerrank/week2/Grid Challenge.cpp
@@ -0,0 +1,18 @@
+string gridChallenge(vector<string> grid)
+{
+    for (int i = 0; i < grid.size(); i++)
+    {
+        sort(grid[i].begin(), grid[i].end());
+    }
+    for (int i = 0; i < grid.size(); i++)
+    {
+        for (int j = 0; j < grid.size() - 1; j++)
+        {
+            if (grid[j][i] > grid[j + 1][i])
+            {
+                return "NO";
+            }
+        }
+    }
+    return "YES";
+}
